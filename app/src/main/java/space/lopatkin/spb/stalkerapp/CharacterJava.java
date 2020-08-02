8d21483abc76786af084f2dc102ff90696409c9d
Lopatkin-spb <lopatkin.spb@mail.ru>
2020-08-02 14:44:46 +0400

    добавлена анимация полоски радиации

--- app/src/main/java/space/lopatkin/spb/stalkerapp/CharacterJava.java
+++ app/src/main/java/space/lopatkin/spb/stalkerapp/CharacterJava.java
@@ -2,6 +2,7 @@ package space.lopatkin.spb.stalkerapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -16,22 +17,85 @@ public class CharacterJava extends AppCompatActivity {
     //damage blood от 0 до 10
     //ввести урон для теста
     int damage = 7;
-    int bloodAfterDamage = 10 - damage;
-
+    //int bloodAfterDamage = 10 - damage;
+    int bloodAfterDamage = 0;
+    int counterBlood = 0;
+    int radiation = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(LAYOUT);
 
+        //навигация
         statusBarOFF();
         backToMainMenuButton();
-        drawBloodBar();
+
+        redrawingBloodBar();
+        redrawingRadiationBar();
+        aidkits();
 
     }
 
+    private void aidkits() {
+        final Button buttonSmallAid = (Button) findViewById(R.id.smallAid);
+        final Button buttonScientificAid = (Button) findViewById(R.id.scientificAid);
+        final Button buttonArmyAid = (Button) findViewById(R.id.armyAid);
 
-    private void drawBloodBar() {
+        buttonSmallAid.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    //блокировка других аптечек во избежание одновременного нажатия
+                    buttonScientificAid.setEnabled(false);
+                    buttonArmyAid.setEnabled(false);
+                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    bloodAfterDamage = bloodAfterDamage + 1;
+                    redrawingBloodBar();
+                }
+                buttonScientificAid.setEnabled(true);
+                buttonArmyAid.setEnabled(true);
+                return false;
+            }
+        });
+        buttonArmyAid.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    //блокировка других аптечек во избежание одновременного нажатия
+                    buttonScientificAid.setEnabled(false);
+                    buttonSmallAid.setEnabled(false);
+                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    bloodAfterDamage = bloodAfterDamage + 2;
+                    redrawingBloodBar();
+                }
+                buttonScientificAid.setEnabled(true);
+                buttonSmallAid.setEnabled(true);
+                return false;
+            }
+        });
+        buttonScientificAid.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    //блокировка других аптечек во избежание одновременного нажатия
+                    buttonSmallAid.setEnabled(false);
+                    buttonArmyAid.setEnabled(false);
+                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    bloodAfterDamage = bloodAfterDamage + 3;
+                    redrawingBloodBar();
+                    radiation = radiation + 4;
+                    redrawingRadiationBar();
+                }
+                buttonSmallAid.setEnabled(true);
+                buttonArmyAid.setEnabled(true);
+                return false;
+            }
+        });
+    }
+
+
+    private void redrawingBloodBar() {
         //массив для прогресса крови
         final int[] progressBlood = {
                 R.id.pointBlood1,
@@ -48,14 +112,41 @@ public class CharacterJava extends AppCompatActivity {
         //закрашиваем прогресс крови темным цветом
         for (int i = 0; i < 10; i++) {
             TextView tv = findViewById(progressBlood[i]);
-            tv.setBackgroundResource(R.drawable.style_points_blood_damage);
+            tv.setBackgroundResource(R.drawable.style_points_blood_off);
         }
         //закрашиваем прогресс крови Светлым цветом
         for (int i = 0; i < bloodAfterDamage; i++) {
             TextView tv = findViewById(progressBlood[i]);
-            tv.setBackgroundResource(R.drawable.style_points_blood);
+            tv.setBackgroundResource(R.drawable.style_points_blood_on);
         }
     }
+
+    private void redrawingRadiationBar() {
+        //массив для прогресса радиации
+        final int[] progressRadiation = {
+                R.id.pointRadiation1,
+                R.id.pointRadiation2,
+                R.id.pointRadiation3,
+                R.id.pointRadiation4,
+                R.id.pointRadiation5,
+                R.id.pointRadiation6,
+                R.id.pointRadiation7,
+                R.id.pointRadiation8,
+                R.id.pointRadiation9,
+                R.id.pointRadiation10
+        };
+        //закрашиваем прогресс радиации темным цветом
+        for (int i = 0; i < 10; i++) {
+            TextView tv = findViewById(progressRadiation[i]);
+            tv.setBackgroundResource(R.drawable.style_points_radiation_off);
+        }
+        //закрашиваем прогресс радиации Светлым цветом
+        for (int i = 0; i < radiation; i++) {
+            TextView tv = findViewById(progressRadiation[i]);
+            tv.setBackgroundResource(R.drawable.style_points_radiation_on);
+        }
+    }
+
 
     private void backToMainMenuButton() {
         //кнопка назад в меню
